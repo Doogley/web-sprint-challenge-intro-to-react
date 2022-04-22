0486daf6e0d85748c8c322f36c851ca3ef52f5b0
Dougy Smith <dougysmith@Dougys-MacBook-Pro.local>
2022-04-22 17:30:40 -0600

    finished sprint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+import StarWarsChar from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +10,26 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [data, setData] = useState([]);
+
+
+useEffect(() => {
+  axios.get("https://swapi.dev/api/people/")
+  .then(res => {
+    setData(res.data);
+  })
+  .catch(err => console.error(err))
+}, [])
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {data.map(element => {
+        return (
+          <StarWarsChar character={element} key={element.hair_color} />
+        )
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Character = styled.div`
+color: red;
+padding: 5px;
+border: 2px solid black;
+`
+
+
+const StarWarsChar = (props) => {
+    return (
+        <Character className='starwarschar'>
+            <h1>
+                {props.character.name}
+            </h1>
+        </Character>
+    )
+}
+
+
+export default StarWarsChar;
